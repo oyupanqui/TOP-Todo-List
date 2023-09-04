dec46ed9fbf23e8e4e532def69ffa780199df05b
oyupanqui <osmar30asis@gmail.com>
2023-09-03 19:50:27 -0500

    added checkStorage

--- src/initialize.js
+++ src/initialize.js
@@ -2,13 +2,14 @@ import updateClock from './date'
 import loadHome from './home'
 import showNav from './showNav'
 import resetContent from './reset'
+import checkStorage from './tasks'
 
 function initialize() {
     updateClock()
     showNav()
     resetContent()
     loadHome()
-    
+    checkStorage()
 }
 
 export default initialize
--- src/tasks.js
+++ src/tasks.js
@@ -15,3 +15,13 @@ function Project (title, color) {
     this.title = title
     this.color = color
 }
+
+function checkStorage () {
+    if (localStorage.length === 0) {
+        myProjects.push(new Project("My Default Project", "#ff0000"))
+        localStorage.setItem("project", JSON.stringify(myProjects))
+    }
+    return localStorage
+}
+
+export default checkStorage
