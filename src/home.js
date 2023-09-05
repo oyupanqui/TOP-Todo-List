bf6014f778ad8589b93ba84f8d5f27b1e8d5c5ae
oyupanqui <osmar30asis@gmail.com>
2023-09-04 21:16:31 -0500

    added functionality to extract local storage

--- src/home.js
+++ src/home.js
@@ -4,6 +4,21 @@ function loadHome() {
     head.textContent = "All Projects"
 
     content.appendChild(head)
+
+    const taskDiv = document.createElement("div")
+    taskDiv.classList.add("task-list")
+
+    const data = JSON.parse(localStorage.getItem("task"))
+    console.log(data)
+
+    data.forEach((d) => {
+        const taskElem = document.createElement("div")
+        taskElem.classList.add("task")
+        taskElem.textContent = d.title
+        taskDiv.appendChild(taskElem)
+    })
+
+    content.appendChild(taskDiv)
 }
 
 export default loadHome
