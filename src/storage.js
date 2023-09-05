d474e6d712ea5635393a51ad239a73ee6b6e8b56
oyupanqui <osmar30asis@gmail.com>
2023-09-04 19:26:58 -0500

    added default project and task

--- src/storage.js
+++ src/storage.js
@@ -5,6 +5,9 @@ export function checkStorage () {
     if (localStorage.length === 0) {
         myProjects.push(new Project("My Default Project", "#ff0000"))
         localStorage.setItem("project", JSON.stringify(myProjects))
+
+        myTasks.push(new Task("Launch of NISAR", "On January 2024 NASA's first radar mission will be launch", "2024-01-01", "high", "a very special day", false, "My Default Project"))
+        localStorage.setItem("task", JSON.stringify(myTasks))
     }
     return localStorage
 }
