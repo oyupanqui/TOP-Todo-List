f670831670bc3bfef0392f9a7020ba39ee3b9f57
oyupanqui <osmar30asis@gmail.com>
2023-09-07 22:46:16 -0500

    added event listener

--- src/form.js
+++ src/form.js
@@ -1,15 +1,16 @@
-function openForm(){
-    formDiv.classList.add("active")
-    formDiv.classList.remove("inactive")
+function openForm(elem){
+    elem.classList.add("active")
+    elem.classList.remove("inactive")
 }
 
-function closeForm(){
-    formDiv.classList.add("deactivating")
-    formDiv.classList.remove("active")
+function closeForm(elem, form){
+    elem.classList.add("deactivating")
+    elem.classList.remove("active")
+    removeBlank()
 
     setTimeout(()=>{
-        formDiv.classList.add("inactive")
-        formDiv.classList.remove("deactivating")
+        elem.classList.add("inactive")
+        elem.classList.remove("deactivating")
         form.reset()
     }, 1000)
 }
@@ -22,13 +23,50 @@ function removeBlank () {
     document.getElementsByClassName("blank")[0].style.display = "none"
 }
 
-function addTaskControl() {
-    const createTaskBtn = document.getElementById("add-task")
-    createTaskBtn.addEventListener("click", function () {
-        openForm()
-        addBlank()
+function formConfirm (elem) {
+    removeBlank()
+    elem.addEventListener("submit", (e) => {
+        e.preventDefault()
+        closeForm()
     })
-    return createTaskBtn
+    return elem
 }
 
-export default addTaskControl
+export function addControlBtns () {
+    const taskBtn = document.getElementById("form-task-div")
+    const createTaskBtn = document.getElementById("add-task")
+    createTaskBtn.addEventListener("click", function () {
+        openForm(taskBtn)
+        addBlank()
+    })
+
+    const projBtn = document.getElementById("form-project-div")
+    const createProjBtn = document.getElementById("add-project")
+    createProjBtn.addEventListener("click", function () {
+        openForm(projBtn)
+        addBlank()
+    })
+    return createTaskBtn, createProjBtn
+}
+
+export function addFormBtns () {
+    const taskForm = document.getElementById("task-form")
+    const projForm = document.getElementById("proj-form")
+
+    const taskFormConfirm = document.getElementById("form-task-confirmation")
+    const projFormConfirm = document.getElementById("form-project-confirmation")
+    formConfirm(taskFormConfirm)
+    formConfirm(projFormConfirm)
+
+    const taskFormCancel = document.getElementById("form-task-cancel")
+    const projFormCancel = document.getElementById("form-project-cancel")
+
+    taskFormCancel.addEventListener("click", () => {
+        closeForm(document.getElementById("form-task-div"), taskForm)
+    })
+        
+    projFormCancel.addEventListener("click", () => {
+        closeForm(document.getElementById("form-project-div"), projForm)
+    })
+    return taskFormConfirm, projFormConfirm, taskFormCancel, projFormCancel
+}
