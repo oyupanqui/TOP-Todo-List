7749c6720142ed906c8c6f91d35afa2205d37770
oyupanqui <osmar30asis@gmail.com>
2023-09-09 16:02:00 -0500

    updated reset task and project

--- src/reset.js
+++ src/reset.js
@@ -1,6 +1,20 @@
-function resetContent () {
+export function resetContent () {
     const content = document.getElementById("content")
     content.childNodes.forEach(n => n.remove())
 }
 
-export default resetContent
+export function resetTaskList () {
+    const taskDiv = document.getElementsByClassName("task-div")
+    let taskLen = taskDiv.length
+    for (let i = 0; i<taskLen; i++) {
+        taskDiv[0].remove()
+    }
+}
+
+export function resetProjectList () {
+    const projectDiv = document.getElementsByClassName("project-div")
+    let projectLen = projectDiv.length
+    for (let i = 0; i<projectLen; i++) {
+        projectDiv[0].remove()
+    }
+}
