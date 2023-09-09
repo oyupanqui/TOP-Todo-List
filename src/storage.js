420d3458ace8cb54e7859a1c33b3dc1badbedb8b
oyupanqui <osmar30asis@gmail.com>
2023-09-09 02:39:18 -0500

    added functions to retrieve data from local storage

--- src/storage.js
+++ src/storage.js
@@ -1,13 +1,34 @@
 import { myProjects, Project } from './projects'
-import { myTasks, Task} from './tasks'
+import { myTasks, Task } from './tasks'
+
+function taskToStorage (task) {
+    myTasks.push(task)
+    localStorage.setItem("task", JSON.stringify(myTasks))
+}
+
+function projectToStorage (project) {
+    myProjects.push(project)
+    localStorage.setItem("project", JSON.stringify(myProjects))
+}
 
 export function checkStorage () {
-    if (localStorage.length === 0) {
-        myProjects.push(new Project("My Default Project", "#ff0000"))
-        localStorage.setItem("project", JSON.stringify(myProjects))
-
+    if (localStorage.getItem("task") === null) {
         myTasks.push(new Task("Launch of NISAR", "On January 2024 NASA's first radar mission will be launch", "2024-01-01", "high", "a very special day", false, "My Default Project"))
         localStorage.setItem("task", JSON.stringify(myTasks))
+    }
+
+    if (localStorage.getItem("project") === null) {
+        myProjects.push(new Project("My Default Project", "#ff0000"))
+        localStorage.setItem("project", JSON.stringify(myProjects))
+    } else {
+        JSON.parse(localStorage.getItem("task")).map(function (task) {
+            let newTask = new Task(task.title, task.description, task.dueDate, task.priority, task.notes, task.check, task.project, task.project.toLowerCase().replaceAll(" ", "-"))
+            return myTasks.push(newTask)
+        })
+        JSON.parse(localStorage.getItem("project")).map(function (proj) {
+            let newProject = new Project(proj.title, proj.color, proj.title.toLowerCase().replaceAll(" ", "-"))
+            return myProjects.push(newProject)
+        })
     }
     return localStorage
 }
@@ -24,7 +45,7 @@ export function addStorageTask () {
         const formProj = document.getElementById("form-task-project").value
         
         const newTask = new Task (formTitle, formDescription, formDate, formPriority, formNotes, formCheck, formProj)
-        console.log(newTask)
+        taskToStorage(newTask)
     })
     return taskConfirm
 }
@@ -37,6 +58,7 @@ export function addStorageProj () {
         
         const newProject = new Project (formTitle, formColor)
         console.log(newProject)
+        projectToStorage(newProject)
     })
     return projConfirm
 }
