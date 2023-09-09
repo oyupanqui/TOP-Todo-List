626154fc7ff1a401df03e27be639bcb973c27251
oyupanqui <osmar30asis@gmail.com>
2023-09-09 16:01:47 -0500

    modified order of functions

--- src/initialize.js
+++ src/initialize.js
@@ -1,7 +1,7 @@
 import updateClock from './date'
 import loadHome from './home'
 import showNav from './showNav'
-import resetContent from './reset'
+import { resetContent } from './reset'
 import { checkStorage } from './storage'
 import { addProjectsNav } from './projects'
 import { addControlBtns, addFormBtns} from './form'
