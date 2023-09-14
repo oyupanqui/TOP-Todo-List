a85c3d1ae7eb1837b65df112d40f082a9437a677
oyupanqui <osmar30asis@gmail.com>
2023-09-14 18:54:57 -0500

    added projects listener

--- src/navigator.js
+++ src/navigator.js
@@ -1,4 +1,5 @@
 import { loadHome } from './home'
+import { loadAllProjects } from './projects'
 
 export function showNav () {
     const navIcon = document.getElementById("sandwich")
@@ -16,6 +17,13 @@ export function showNav () {
 export function homeListener () {
     const allTasks = document.getElementById("all")
     allTasks.addEventListener("click", function () {
-        loadHome()
+        loadHome(false)
+    })
+}
+
+export function projectsListener () {
+    const allProjects = document.getElementById("all-projects")
+    allProjects.addEventListener("click", function () {
+        loadAllProjects()
     })
 }
