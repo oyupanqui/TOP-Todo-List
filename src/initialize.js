13634e6bf5e6490215dfe066dc841130112b1a2d
oyupanqui <osmar30asis@gmail.com>
2023-09-13 21:10:18 -0500

    updated the initialize function to pull data first

--- src/initialize.js
+++ src/initialize.js
@@ -2,8 +2,10 @@ import { updateClock } from './date'
 import { loadHome } from './home'
 import { showNav } from './showNav'
 import { addSubmitBtns, addFormBtns} from './form'
+import { pullData } from './storage'
 
 function initialize() {
+    pullData()
     updateClock()
     showNav()
     addSubmitBtns()
