58ad892ea191cad1c5bdc04e389757f7358ec8cc
oyupanqui <osmar30asis@gmail.com>
2023-09-15 20:53:29 -0500

    created today js

---
+++ src/today.js
@@ -0,0 +1,31 @@
+import { myTasks } from "./tasks";
+import { pullData } from "./storage";
+import { resetContent } from "./reset";
+import { addTasks } from "./tasks";
+import { format } from "date-fns";
+
+function todayEventListener () {
+    pullData()
+    resetContent()
+    const content = document.getElementById("content")
+    const head = document.createElement("h2")
+    head.textContent = "Today's Tasks"
+
+    content.appendChild(head)
+
+    const taskDiv = document.createElement("div")
+    taskDiv.classList.add("task-list")
+
+    addTasks(myTasks.filter(filterDate), taskDiv)
+
+    content.appendChild(taskDiv)
+}
+
+export function addTodayTasks () {
+    const today = document.getElementById("today")
+    today.addEventListener("click", todayEventListener)
+}
+
+function filterDate (item) {
+    return item.dueDate === format(new Date, 'yyyy-MM-dd')
+}
